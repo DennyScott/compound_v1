c8ab35887d2b16ea37cd85b1cfe369b8552bd3ba
Denny Scott <dennyscott301@gmail.com>
2014-11-30 15:40:23 -0600

    Added whiteboard and iron router

--- client/views/whiteboard/compound.js
+++ client/views/whiteboard/compound.js
@@ -1,23 +1,18 @@
-if (Meteor.isClient) {
+
   // counter starts at 0
   Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
+  Template.whiteboard.helpers({
     counter: function () {
       return Session.get("counter");
     }
   });
 
-  Template.hello.events({
+  Template.whiteboard.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set("counter", Session.get("counter") + 1);
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+
