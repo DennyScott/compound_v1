b3f44f10e36defb22f1a93fd7c92d87b661df866
Travis Scott <travisscott301@gmail.com>
2014-11-30 16:09:11 -0600

    Added Projects collection to app

--- client/views/whiteboard/compound.js
+++ client/views/whiteboard/compound.js
@@ -1,18 +1,15 @@
+// counter starts at 0
+Session.setDefault("counter", 0);
 
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+Template.whiteboard.helpers({
+  counter: function() {
+    return Session.get("counter");
+  }
+});
 
-  Template.whiteboard.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.whiteboard.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
-
-
+Template.whiteboard.events({
+  'click button': function() {
+    // increment the counter when button is clicked
+    Session.set("counter", Session.get("counter") + 1);
+  }
+});
