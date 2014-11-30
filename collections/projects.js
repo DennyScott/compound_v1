506c21770a8ef6c93cda56e5e988d94ecbcdd374
Travis Scott <travisscott301@gmail.com>
2014-11-30 16:42:28 -0600

    Added meteor method for adding projects to app

--- collections/projects.js
+++ collections/projects.js
@@ -1,1 +1,20 @@
 Projects = new Mongo.Collection('projects');
+
+Meteor.methods({
+	projectInsert: function(projectAttributes) {
+		check(Meteor.userId(), String);
+		check(projectAttributes, {
+			description: String
+		});
+		var user = Meteor.user();
+		var project = _.extend(_.pick(projectAttributes, 'description'), {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date()
+		});
+		var projectId = Projects.insert(project);
+		return {
+			_id: projectId
+		};
+	}
+});
